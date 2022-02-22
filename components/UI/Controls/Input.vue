b431a27e4993f64dd6f4e3044188186fc5ae411e
scripac <mukhammadiev013@gmail.com>
2022-02-23 02:20:16 +0500

    141 frontend end

--- components/UI/Controls/Input.vue
+++ components/UI/Controls/Input.vue
@@ -5,6 +5,7 @@
       v-bind="$attrs"
       :value="value"
       :type="type"
+      :required="required"
       @input="$emit('input', $event.target.value)">
   </div>
 </template>
@@ -20,6 +21,10 @@
       type: {
         type: String,
         default: 'text'
+      },
+      required: {
+        type: Boolean,
+        default: true
       }
     }
   }
--- layouts/admin.vue
+++ layouts/admin.vue
@@ -18,7 +18,8 @@
 
   export default {
     name: 'admin',
-    components: {Header}
+    components: {Header},
+    middleware: ['auth']
   }
 </script>
 
