72bddf0aa17d90871107ad48a2d8658c945b02e4
scripac <mukhammadiev013@gmail.com>
2022-02-22 17:47:30 +0500

    135

--- components/system/Header.vue
+++ components/system/Header.vue
@@ -3,7 +3,7 @@
     <div class="navbar">
       <div class="container">
         <div class="navbar-content">
-          <div class="logo">Blog</div>
+          <div class="logo"><nuxt-link to="/">Blog</nuxt-link></div>
           <ul class="navbar-list">
             <li class="navbar-item" v-for="link of links" :key="link.title">
               <nuxt-link class="navbar-link" :title="link.title" :to="link.url">{{ link.title }}</nuxt-link>
@@ -21,7 +21,7 @@
     data() {
       return {
         links: [
-          {title: 'Home', url: '/'},
+          {title: 'Blog', url: '/blog'},
           {title: 'About', url: '/about'}
         ]
       }
@@ -30,6 +30,10 @@
 </script>
 
 <style lang="scss">
+
+  .logo a {
+    color: #333;
+  }
 
   .navbar-link {
     &.nuxt-link-exact-active {
---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+	<section class="not-found">
+    <div class="container">
+      <div class="not-found__wrapper">
+        <p class="title">Page Not Found</p>
+        <nuxt-link to="/">Back to main page</nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'error'
+  }
+</script>
+
+<style lang="scss">
+
+  .not-found__wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 80vh;
+  }
+
+</style>
