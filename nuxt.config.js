72a461ffe6907f346a2e044c51b69821ff98dc0f
scripac <mukhammadiev013@gmail.com>
2022-02-24 12:21:58 +0500

    '160'

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,9 @@ export default {
     },
     meta: [
       {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'}
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {name: 'yandex-verification', content: 'KEY'},
+      {name: 'google-site-verification', content: 'KEY'}
     ],
     link: [
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
@@ -21,7 +23,20 @@ export default {
     '~plugins/app-components.js'
   ],
   components: true,
-  buildModules: [],
-  modules: [],
+  buildModules: [
+    // ['@nuxtjs/google-analytics', {
+    //   id: 'UA-KEY'
+    // }]
+  ],
+  modules: [
+    // ['@nuxtjs/yandex-metrika', {
+    //     id: 'KEY',
+    //     webvisor: false,
+    //     clickmap: true,
+    //     // useCDN: false,
+    //     trackLinks: true,
+    //     // accurateTrackBounce: true
+    //   }]
+  ],
   build: {}
 }
